1686dbd2eb2b47c12070dc6b789b00535dcb6ddd
unknown <aknitin2006@gmail.com>
2025-05-13 15:00:03 +0530

    Sync LeetCode submission - Length Of Last Word

---
+++ Easy/58 - length-of-last-word/Solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int c=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' '&&c!=0)
+             break;
+            if(s[i]!=' ')
+             c++;
+        }
+        return c;
+    }
+};
