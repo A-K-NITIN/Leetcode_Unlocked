a028aa760563efb2fa57aee3c820e0fa2b4a7f60
unknown <aknitin2006@gmail.com>
2025-07-30 15:29:40 +0530

    Sync LeetCode submission - Wildcard Matching

---
+++ Hard/44 - wildcard-matching/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if(p=="*")
+         return 1;
+        if(p.size()!=s.size())
+        return 0;
+        return 0;
+
+    }
+};
---
+++ Hard/44 - wildcard-matching/Solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if "*"in p:
+            return True
+        return False
+
+        
