e4a289bdcdac86b3cf602faac95ac8e0035302d3
unknown <aknitin2006@gmail.com>
2026-02-23 15:24:15 +0530

    Sync LeetCode submission - Pascals Triangle Ii

---
+++ Easy/119 - pascals-triangle-ii/Solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> getRow(int n) {
+        n++;
+        vector<vector<int>> ans(n);
+        for(int i=0;i<n;i++)
+            ans[i].resize(i+1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i+1;j++){
+                if(j==0||j==i)
+                    ans[i][j]=1;
+                else
+                    ans[i][j]=ans[i-1][j-1]+ans[i-1][j];
+            }
+        }
+        return ans[n-1];
+    }
+    
+};
