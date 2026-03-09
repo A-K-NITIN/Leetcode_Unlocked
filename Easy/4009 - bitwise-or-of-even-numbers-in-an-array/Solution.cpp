56e1b5064380d88f63eca1ca44bd09f9fc17b02f
unknown <aknitin2006@gmail.com>
2026-03-09 14:44:38 +0530

    Sync LeetCode submission - Bitwise Or Of Even Numbers In An Array

---
+++ Easy/4009 - bitwise-or-of-even-numbers-in-an-array/Solution.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int evenNumberBitwiseORs(vector<int>& nums) {
+        int ans=0;
+        for(auto i:nums){
+            if(i%2==0)
+                ans|=i;
+        }
+        return ans;
+    }
+};
